d11e0503bd8b9ca73383c694f8761f3aa3940d3c
Wonjin Cho <cwonjin1@gmail.com>
2021-10-19 22:16:17 -0400

    add js solution with pseudocode

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -1,6 +1,25 @@
 function reverseString(str) {
   // type your code here
+  
+  //***********************************************************/
+  // initialize a variable called reversed with an empty string
+  let reversed = '';
+
+  // iterate backward through the input string
+  // for(i = str.length - 1; i > -1; --i) {
+  for(let i = str.length - 1; i >=0; i--) {
+    console.log(str, "<===")
+  // set reversed to reversed + current character
+    reversed = reversed + str[i];
+  }
+  // return reversed
+  return reversed;
+
 }
+console.log("=>", reverseString("abcd"));
+  //***********************************************************/
+
+
 
 if (require.main === module) {
   // add your own tests in here
