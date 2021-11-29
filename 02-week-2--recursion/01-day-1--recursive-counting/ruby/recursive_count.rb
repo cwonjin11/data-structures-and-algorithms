3be8fb9a923ed630b0b76c6c83c8f9ef1b5b684f
Wonjin Cho <cwonjin1@gmail.com>
2021-11-29 11:26:20 -0500

    add ruby solution

--- 02-week-2--recursion/01-day-1--recursive-counting/ruby/recursive_count.rb
+++ 02-week-2--recursion/01-day-1--recursive-counting/ruby/recursive_count.rb
@@ -1,5 +1,9 @@
 def recursive_count(num = 0)
   # type your code in here
+return if num >= 10
+puts num
+recursive_count(num + 1)
+
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -8,4 +12,5 @@ end
 
 # OPTIONAL
 # Please add your pseudocode to this file
+
 # And a written explanation of your solution
