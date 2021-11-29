ba522fa189a4ec64508fb01ad7b0e0bc8eb0a5a1
Wonjin Cho <cwonjin1@gmail.com>
2021-11-29 11:59:43 -0500

    add pseudocode for js solution

--- 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
+++ 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
@@ -1,10 +1,13 @@
 function recursiveCount(num = 0) {
   // type your code here
-  if(num >=10) {
-    return;
-  }
-  console.log(num)
-  recursiveCount(num + 1)
+//base case
+if (num >=10) {
+  return;
+}
+//Pass arguments into the function that immediately trigger the base case
+console.log(num)
+//Pass the next arguments that trigger the recursive call just once
+recursiveCount(num + 1)
 }
 
 if (require.main === module) {
