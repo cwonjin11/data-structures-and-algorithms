2189e8c77b39719df869dc20d55d0e9141b1a4ee
Wonjin Cho <cwonjin1@gmail.com>
2022-02-01 15:12:35 -0500

    Modified: Recursive solution

--- 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
+++ 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
@@ -1,15 +1,17 @@
-function recursiveCount(num = 0) {
+function recursiveCount(num =0 ) {
   // type your code here
 //base case
 if (num >=10) {
-  return;
+  // console.log(return);
+  return 
 }
 //Pass arguments into the function that immediately trigger the base case
 console.log(num)
 //Pass the next arguments that trigger the recursive call just once
-recursiveCount(num + 1)
+recursiveCount(++num)
+// console.log(num)
 }
-
+// recursiveCount(num = 10)
 if (require.main === module) {
   recursiveCount();
 }
