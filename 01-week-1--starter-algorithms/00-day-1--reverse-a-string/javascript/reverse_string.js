283b4ce676ae6ad6c55cda0a7429b717c509453b
Wonjin Cho <cwonjin1@gmail.com>
2022-02-01 10:16:55 -0500

    Modified: Solution modified with backward for loop

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -1,28 +1,45 @@
-function reverseString(str) {
-  // type your code here
+// function reverseString(str) {
+//   // type your code here
   
+//   //***********************************************************/
+//   // initialize a variable called reversed with an empty string
+//   let reversed = '';
+//   // iterate backward through the input string
+//   // for(i = str.length - 1; i > -1; --i) {
+//   for(let i = str.length - 1; i >=0; i--) {
+//     // console.log(str, "<===")
+//     // console.log(str.length)
+//     // console.log(str[3])
+//     // console.log(str[2])
+//     // console.log(str[1])
+//     // console.log(str[0])
+//   // set reversed to reversed + current character
+//     reversed = reversed + str[i];
+//   }
+//   // return reversed
+//   return reversed;
+
+// }
+// console.log("=>", reverseString("abcd"));
   //***********************************************************/
-  // initialize a variable called reversed with an empty string
-  let reversed = '';
-  // iterate backward through the input string
-  // for(i = str.length - 1; i > -1; --i) {
-  for(let i = str.length - 1; i >=0; i--) {
-    // console.log(str, "<===")
-    // console.log(str.length)
-    // console.log(str[3])
-    // console.log(str[2])
-    // console.log(str[1])
-    // console.log(str[0])
-  // set reversed to reversed + current character
-    reversed = reversed + str[i];
+
+  // question : 
+    //reverse a string. Your method receives an argument, string, and output that string with its letters in reverse order
+    //'abc' => 'cba', '123' => '321', 'aaa=>aaa'
+    //pseudocode
+  const reverseString = (str) => {
+
+    let output = ""
+
+      for(let i = str.length-1; i > -1  ; --i){
+        // keep adding str[i] to output
+        output += str[i]
+      }
+
+    return output
   }
-  // return reversed
-  return reversed;
 
-}
-console.log("=>", reverseString("abcd"));
-  //***********************************************************/
-
+  console.log(reverseString('123'))
 
 
 if (require.main === module) {
