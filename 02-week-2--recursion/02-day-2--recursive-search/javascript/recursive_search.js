75c9ad7cc10a74fc9bb15b6ecd7b01abba8e4d4e
Wonjin Cho <cwonjin1@gmail.com>
2022-02-01 15:42:04 -0500

    Modified: Used typical for loop vs recursive

--- 02-week-2--recursion/02-day-2--recursive-search/javascript/recursive_search.js
+++ 02-week-2--recursion/02-day-2--recursive-search/javascript/recursive_search.js
@@ -1,15 +1,86 @@
-function recursiveSearch(arr, target) {
-  // type your code here
+/*
+  given an array, find the target value. 
+  if found, return true, otherwise, return false
+ */
 
-  if (arr.includes(target)) {
+const recursiveCount = require("../../01-day-1--recursive-counting/javascript/recursive_count")
+
+// const recursiveCount = require("../../01-day-1--recursive-counting/javascript/recursive_count");
+
+  /* 
+  input [1,2,3], 2
+  output true 
+
+  input [1,2,3,4], 5
+  output false 
+  */
+
+  /*
+  for loop : iterate each element, 
+    if arr[i] == target value
     return true
-  } 
-  if (arr.length === 0 || !arr.includes(target)) {
-    return false
-  } 
 
-  return recursiveSearch(arr, target)
+    else return false
+  */
+
+function recursiveSearch(arr, target) {
+
+  // for(let i=0; i<arr.length; i++){
+  //   if(arr[i] === target){
+  //     return true
+  //   }
+  // }
+  // return false
+
+  if (arr.length === 0) {
+    return false
+  }
+
+  if (arr[0] === target){
+    return true
+  }
+
+  return recursiveSearch(arr.slice(1), target)
 }
+
+console.log(recursiveSearch([1,2,3], 2))
+console.log(recursiveSearch([1,2,3,4], 5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function recursiveSearch(arr, target) {
+//   // type your code here
+
+//   if (arr.includes(target)) {
+//     return true
+//   } 
+//   if (arr.length === 0 || !arr.includes(target)) {
+//     return false
+//   } 
+
+//   return recursiveSearch(arr, target)
+// }
 
 if (require.main === module) {
   // add your own tests in here
