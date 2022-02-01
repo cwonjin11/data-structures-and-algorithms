a51342bc5f3bf84ebf009acb83b24a64c9ecbed0
Wonjin Cho <cwonjin1@gmail.com>
2022-02-01 12:13:17 -0500

    Added: New solution using Set object with has and add method

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -1,19 +1,88 @@
-function findFirstDuplicate(arr) {
-  // initilize an empty set called uniques
-  const uniques = new Set()
-  //iterate through the input arr
-    for (const value of arr) {
-      // if the value is in uniques, return the value
-      if (uniques.has(value)) {
-        return value;
-      }
-      // else add value to the set
-      uniques.add(value)
-    }
 
-    // return -1 if no duplicate found during iteration
-    return -1;
+
+// const findFirstDuplicate = (arr) => {
+//     const result = []
+//       for (let i=0; i < arr.length; i++){
+//         if(result.includes(arr[i])){
+//           return arr[i]
+//         } else {
+//         result.push(arr[i])}
+//       }
+//     return -1
+// }
+// console.log(findFirstDuplicate([2, 1, 3, 2, 3]))
+
+/*
+Problem : Find first duplicate value from a given array
+  if no duplicates => return -1
+
+  pseudocode
+  A Set is a data structure that contains only an unique values/objects
+  If I check a value is in a Set, I will know if there is a duplicate
+  If there is a duplicate, I will return in right away, otherwise return -1 if no dupe
+*/
+
+// const findFirstDuplicate = (arr) => {
+//   const uniq = new Set()
+//     for (let i=0; i<arr.length; i++){
+//       if(uniq.has(arr[i])){
+//         return arr[i]
+//       } else {
+//         uniq.add(arr[i])
+//       }
+//     }
+//   return -1
+// }
+// console.log(findFirstDuplicate([2, 1, 3, 2, 3]))
+
+const findFirstDuplicate = (arr) => {
+  const uniq = new Set()
+    for (const value of arr){
+      if(uniq.has(value)){
+       return value
+      } 
+      uniq.add(value)
+    }
+  return -1
 }
+console.log(findFirstDuplicate([2, 1, 3, 2, 3]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function findFirstDuplicate(arr) {
+//   // initilize an empty set called uniques
+//   const uniques = new Set()
+//   //iterate through the input arr
+//     for (const value of arr) {
+//       // if the value is in uniques, return the value
+//       if (uniques.has(value)) {
+//         return value;
+//       }
+//       // else add value to the set
+//       uniques.add(value)
+//     }
+
+//     // return -1 if no duplicate found during iteration
+//     return -1;
+// }
 
 
 
