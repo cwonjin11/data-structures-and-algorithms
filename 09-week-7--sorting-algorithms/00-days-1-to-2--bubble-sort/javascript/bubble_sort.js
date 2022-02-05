acdc300f508a37ca324b4d9cdfd01b6f02b42cff
Wonjin Cho <cwonjin1@gmail.com>
2022-02-04 22:14:40 -0500

    Bubble sort solution added by using nested for loop

--- 09-week-7--sorting-algorithms/00-days-1-to-2--bubble-sort/javascript/bubble_sort.js
+++ 09-week-7--sorting-algorithms/00-days-1-to-2--bubble-sort/javascript/bubble_sort.js
@@ -1,6 +1,34 @@
 function bubbleSort(arr) {
   // type your code here
-}
+  for(let i = 0; i < arr.length -1 ; i++){
+    for(let j = 0; j < arr.length -1 -i; j++){
+      if (arr[j] > arr[j+1]){
+        [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+      }
+    }
+  }
+  return arr
+
+  
+  // let sorted = false;
+
+  // while (!sorted) {
+  //   sorted = true;
+
+  //   arr.forEach((num, idx) => {
+  //     if (idx === arr.length - 1) {
+  //       return;
+  //     }
+
+  //     if (num > arr[idx + 1]) {
+  //       [arr[idx], arr[idx + 1]] = [arr[idx + 1], arr[idx]];
+  //       sorted = false;
+  //     }
+  //   });
+  }
+
+
+// console.log(bubbleSort([13,2,4,5,1,7,8,88,76]))
 
 if (require.main === module) {
   // add your own tests in here
